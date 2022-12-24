38dd61e49f42935a9b293d5caf93b00de763194b
ketanpipaliya <ketan@siddhisai.net>
2022-12-24 13:37:27 +0530

    jhello

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+export const config = {
+  api: {
+    responseLimit: false,
+  },
+}
 
 const request = require("request");
 
